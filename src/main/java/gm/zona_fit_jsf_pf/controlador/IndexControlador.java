c5f834b3af2babf06fb845e6bae5f0e2b0f45e41
Samuel Lopez <samuellopez01@hotmail.com>
2024-12-13 10:02:53 -0500

    Modal Agregar y Guardar cliente

--- src/main/java/gm/zona_fit_jsf_pf/controlador/IndexControlador.java
+++ src/main/java/gm/zona_fit_jsf_pf/controlador/IndexControlador.java
@@ -3,8 +3,11 @@ package gm.zona_fit_jsf_pf.controlador;
 import gm.zona_fit_jsf_pf.modelo.Cliente;
 import gm.zona_fit_jsf_pf.servicio.IClienteServicio;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +23,7 @@ public class IndexControlador {
     @Autowired
     IClienteServicio clienteServicio;
     private List<Cliente> clientes;
+    private Cliente clienteSeleccionado;
     private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
 
     @PostConstruct
@@ -30,5 +34,29 @@ public class IndexControlador {
     public void cargarDatos(){
         this.clientes = this.clienteServicio.listarClientes();
         this.clientes.forEach(cliente -> logger.info(cliente.toString()));
+    }
+
+    public void agregarCliente(){
+        this.clienteSeleccionado = new Cliente();
+    }
+
+    public void guardarCliente(){
+        logger.info("cliente a guardar " + this.clienteSeleccionado);
+//        Agregar
+        if (this.clienteSeleccionado.getId() == null){
+            this.clienteServicio.guardarCliente(this.clienteSeleccionado);
+            this.clientes.add(this.clienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cliente agregado"));
+        }
+
+//        Ocultar ventana modal
+        PrimeFaces.current().executeScript("PF('ventanaModalCliente').hide()");
+
+//        Actualizar tabla usando ajax
+        PrimeFaces.current().ajax().update("forma-clientes:mensajes", "forma-clientes:clientes-tabla");
+
+//        Reset del objeto cliente seleccionado
+        this.clienteSeleccionado = null;
+
     }
 }
